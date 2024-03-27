f76a6aa8a95e0c52573c398125afa3cde464d091
shashank2907908 <shashanknalogal@gmail.com>
2024-03-27 23:48:06 +0530

    game board commit

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,12 +3,13 @@ import { useState } from "react";
 export default function Player({ initialName, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
   const [playerName, setPlayerName] = useState(initialName);
+  
   function handleClick() {
     setIsEditing((editing) => !editing);
   }
 
   function handleChange(event) {
-    setPlayerName(event.target.value)
+    setPlayerName(event.target.value);
   }
 
   let editablePlayerName = <span className="player-name">{playerName}</span>;
