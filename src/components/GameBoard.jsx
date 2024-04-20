de17db60c83f9719c896aeaec63424ccfacb096d
= <=>
2024-04-20 17:07:23 +0530

    lifting StateUp commit

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,23 +1,35 @@
+import { useState } from "react";
+
 const initialGameBoard = [
-  [null, "X", "O"],
   [null, null, null],
-  [null, "X", "O"],
+  [null, null, null],
+  [null, null, null],
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updated = [...prevGameBoard.map((innerArray) => [...innerArray]) ];
+      updated[rowIndex][colIndex] = "X";
+      return updated;
+    });
+  }
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
         </li>
       ))}
-    </ol>   
+    </ol>
   );
 }
